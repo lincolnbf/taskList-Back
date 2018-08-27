97078c3a256d757ef43a2eadcf93d4bd5ea45f14
lincolnbf <lincolnbf@gmail.com>
2018-08-27 09:50:58 -0300

    Iniciando o teste prático da Supero

---
+++ src/main/java/com/lincolnbf/cursomc/SuperoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.lincolnbf.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperoBackendApplication.class, args);
+	}
+}
